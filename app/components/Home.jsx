f3c9d9e2dc9bf72668f40f3f054700c414aa9e57
Stephan Ahlf <stephan.ahlf@googlemail.com>
2018-05-21 09:52:03 +0200

    fix git status component

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -6,33 +6,11 @@ import 'font-awesome/css/font-awesome.min.css'
 import XTerminal from './XTerminal.jsx'
 import Git from './git.jsx'
 
-const chalk = require('chalk');
-const chalkOptions = { enabled: true, level: 2 };
-const forcedChalk = new chalk.constructor(chalkOptions);
-const EventEmitter = require('events')
+const chalk = require('chalk')
+const chalkOptions = { enabled: true, level: 2 }
+const forcedChalk = new chalk.constructor(chalkOptions)
 const path = require('path')
 const fs = require('fs')
-const branch = require('git-branch')
-const isGitClean = require('is-git-clean')
-const shell = require('shelljs')
-
-const findup = function (basedir, filename) {
-  var result = null;
-  if (fs.existsSync(path.join(basedir, filename))) {
-    result = {
-      dir: basedir,
-      filename: filename,
-      fullpath: path.join(basedir, filename)
-    };
-  } else {
-    var newdir = path.join(basedir, "..");
-    if (newdir !== basedir) {
-      result = findup(newdir, filename);
-    }
-  }
-
-  return result;
-};
 
 const theme = {
   foreground: '#2ee2a6',
@@ -65,11 +43,8 @@ export default class Home extends Component<Props> {
     super()
     this.state = {
       theme,
-      version: require('electron').remote.app.getVersion(),
-      gitInfo: ''
+      version: require('electron').remote.app.getVersion()
     }
-
-    this.emitter = new EventEmitter()
   }
 
   componentDidMount() {
@@ -77,49 +52,17 @@ export default class Home extends Component<Props> {
   }
 
 
-
   onDirectoryChange(info) {
     const self = this
     if (info.local) {
       window.document.title = info.path
 
-      self.setState({
-        fullpath: info.path
-      })
-
-      const gitFolder = findup(info.path, '.git')
-      if (gitFolder && fs.existsSync(gitFolder.fullpath)) {
-        const err = function (err) {
-          self.setState({
-            gitInfo: err.message
-          })
-          console.error(err)
-        }
-        branch(gitFolder.fullpath)
-          .then(name => {
-            const clean = isGitClean(info.path).then(function (clean) {
-
-              const color = (clean ? self.state.theme.brightGreen : self.state.theme.brightRed)
-              const gitStatus = (clean ? `${name} (clean)` : `${name} (not clean)`)
-              self.setState({
-                gitInfo: gitStatus,
-                gitInfoColor: color,
-                titleColor: color
-              })
-            }).catch(err)
-          }).catch(err)
-      } else {
-        this.setState({
-          gitInfo: '',
-          titleColor: self.state.theme.brightBlue
-        })
-      }
-
+      this.setState({ fullpath: info.path })
     } else {
       window.document.title = '[' + info.path + ']'
       this.setState({
-        gitInfo: '',
-        titleColor: this.state.theme.brightRed
+        titleColor: this.state.theme.brightRed,
+        fullpath: info.path
       })
     }
   }
@@ -145,28 +88,19 @@ export default class Home extends Component<Props> {
 
     const renderFooter = () => {
       return (
-        <div className="row">
-          <div className="col">
-            {renderGitInfo()}
-          </div>
-          <div className="col">
-            {renderNpmInfo()}
-          </div>
-          <div className="col">
-            <Git fullpath={this.state.fullpath} />
+        <div className="">
+          <div className="row">
+            <div className="col-md-4">
+              <Git fullpath={this.state.fullpath} theme={this.state.theme} />
+            </div>
+            <div className="col-md-4">
+              {renderNpmInfo()}
+            </div>
+            <div className="col-md-4">
+            </div>
           </div>
         </div>
       )
-    }
-    const renderGitInfo = () => {
-      if (this.state.gitInfo) {
-        return (
-          <div>
-            <svg width="18" className="footer-status-icon" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet"><path d="M251.172 116.594L139.4 4.828c-6.433-6.437-16.873-6.437-23.314 0l-23.21 23.21 29.443 29.443c6.842-2.312 14.688-.761 20.142 4.693 5.48 5.489 7.02 13.402 4.652 20.266l28.375 28.376c6.865-2.365 14.786-.835 20.269 4.657 7.663 7.66 7.663 20.075 0 27.74-7.665 7.666-20.08 7.666-27.749 0-5.764-5.77-7.188-14.235-4.27-21.336l-26.462-26.462-.003 69.637a19.82 19.82 0 0 1 5.188 3.71c7.663 7.66 7.663 20.076 0 27.747-7.665 7.662-20.086 7.662-27.74 0-7.663-7.671-7.663-20.086 0-27.746a19.654 19.654 0 0 1 6.421-4.281V94.196a19.378 19.378 0 0 1-6.421-4.281c-5.806-5.798-7.202-14.317-4.227-21.446L81.47 39.442l-76.64 76.635c-6.44 6.443-6.44 16.884 0 23.322l111.774 111.768c6.435 6.438 16.873 6.438 23.316 0l111.251-111.249c6.438-6.44 6.438-16.887 0-23.324" fill="#DE4C36" /></svg>
-            {this.state.gitInfo}
-          </div>
-        )
-      }
     }
 
     const renderNpmInfo = () => {
@@ -188,8 +122,8 @@ export default class Home extends Component<Props> {
       <div ref={(e) => { this.main = e }} className="container-fluid d-flex h-100 flex-column">
         <div className="row flex-shrink-0">
           <div className="col">
-            <div className="xcmd-title" style={{ WebkitAppRegion: 'drag', fontFamily: 'courier-new, courier, monospace', color: this.state.titleColor }}>
-              $ xcmd Version [{this.state.version}]
+            <div className="xcmd-title" style={{ WebkitAppRegion: 'drag', fontFamily: 'courier-new, courier, monospace', color: this.state.theme.white }}>
+              xcmd v{this.state.version}
             </div>
           </div>
         </div>
@@ -206,6 +140,6 @@ export default class Home extends Component<Props> {
           </div>
         </div>
       </div >
-    );
+    )
   }
 }
--- app/components/git.jsx
+++ app/components/git.jsx
@@ -58,7 +58,7 @@ export default class Git extends Component<Props> {
             }
         })
     } */
-    componentWillUpdate() {
+    componentDidUpdate() {
         if (this.state.fullpath !== this.props.fullpath) {
             this.fetchDirectoryInformation()
         }
@@ -69,7 +69,8 @@ export default class Git extends Component<Props> {
         const state = function (msg) {
             self.state.fullpath = self.props.fullpath
             self.setState({
-                gitInfo: msg
+                gitInfo: msg,
+                fullpath: self.props.fullpath
             })
             console.error(msg)
         }
@@ -78,7 +79,7 @@ export default class Git extends Component<Props> {
             if (gitFolder) {
                 const currentBranch = branch.sync(gitFolder.fullpath)
                 const clean = isGitClean.sync(self.props.fullpath)
-                const color = 'red' // (clean ? self.props.parent.state.theme.brightGreen : self.props.parent.state.theme.brightRed)
+                const color = (clean ? self.props.theme.green : self.props.theme.red)
                 const gitStatus = (clean ? `${currentBranch} (clean)` : `${currentBranch} (not clean)`)
                 self.setState({
                     gitInfo: gitStatus,
@@ -86,28 +87,23 @@ export default class Git extends Component<Props> {
                     fullpath: this.props.fullpath
                 })
             } else {
-                state('')
+                state(null)
             }
         } else {
-            state('')
+            state(null)
         }
     }
 
     render() {
-
         if (this.state.gitInfo) {
             return (
-                <div>
+                <div style={{ color: this.state.gitInfoColor }}>
                     <svg width="18" className="footer-status-icon" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet"><path d="M251.172 116.594L139.4 4.828c-6.433-6.437-16.873-6.437-23.314 0l-23.21 23.21 29.443 29.443c6.842-2.312 14.688-.761 20.142 4.693 5.48 5.489 7.02 13.402 4.652 20.266l28.375 28.376c6.865-2.365 14.786-.835 20.269 4.657 7.663 7.66 7.663 20.075 0 27.74-7.665 7.666-20.08 7.666-27.749 0-5.764-5.77-7.188-14.235-4.27-21.336l-26.462-26.462-.003 69.637a19.82 19.82 0 0 1 5.188 3.71c7.663 7.66 7.663 20.076 0 27.747-7.665 7.662-20.086 7.662-27.74 0-7.663-7.671-7.663-20.086 0-27.746a19.654 19.654 0 0 1 6.421-4.281V94.196a19.378 19.378 0 0 1-6.421-4.281c-5.806-5.798-7.202-14.317-4.227-21.446L81.47 39.442l-76.64 76.635c-6.44 6.443-6.44 16.884 0 23.322l111.774 111.768c6.435 6.438 16.873 6.438 23.316 0l111.251-111.249c6.438-6.44 6.438-16.887 0-23.324" fill="#DE4C36" /></svg>
                     {this.state.gitInfo}
                 </div>
             )
         } else {
-            return (
-                <div>
-                    {this.state.fullpath}
-                </div>
-            )
+            return null
         }
     }
 }
